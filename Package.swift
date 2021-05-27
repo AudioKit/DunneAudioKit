badcc9f60fd9d7b437e36d3cc6bc713bb52e3fb4
Aurelius Prochazka <aurephd@gmail.com>
2021-05-27 11:45:09 -0700

    Cleaned up Package.swift

--- Package.swift
+++ Package.swift
@@ -5,46 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "DunneAudioKit",
-    platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "DunneAudioKit",
-            targets: ["DunneAudioKit"]),
-    ],
+    platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
+    products: [.library(name: "DunneAudioKit", targets: ["DunneAudioKit"])],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/AudioKit/KissFFT", .branch("main")),
-        .package(url: "https://github.com/AudioKit/AudioKit", .branch("develop")),
-
+        .package(url: "https://github.com/AudioKit/AudioKit", .branch("develop"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "DunneAudioKit",
-            dependencies: ["AudioKit", "CDunneAudioKit"]),
+        .target(name: "DunneAudioKit", dependencies: ["AudioKit", "CDunneAudioKit"]),
         .target(
             name: "CDunneAudioKit",
             dependencies: ["AudioKit", "KissFFT"],
             exclude: [
-                "AudioKitCore/Modulated Delay/README.md",
                 "AudioKitCore/Sampler/Wavpack/license.txt",
                 "AudioKitCore/Common/README.md",
                 "AudioKitCore/Common/Envelope.hpp",
                 "AudioKitCore/Sampler/README.md",
                 "AudioKitCore/README.md",
             ],
-            publicHeadersPath: "include",
-            cxxSettings: [
-                .headerSearchPath("AudioKitCore/Common"),
-                .headerSearchPath(".")]),
-        .testTarget(
-            name: "DunneAudioKitTests",
-            dependencies: ["DunneAudioKit"],
-            resources: [.copy("TestResources/")]),
+            cxxSettings: [.headerSearchPath("AudioKitCore/Common")]),
+        .testTarget(name: "DunneAudioKitTests", dependencies: ["DunneAudioKit"], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: .cxx14
 )
