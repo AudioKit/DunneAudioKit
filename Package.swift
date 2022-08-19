bb06db3dd5891dc71ece6c7b2c79f02ee25b18c8
Aurelius Prochazka <aurephd@gmail.com>
2022-08-18 18:03:30 -0700

    Updated to latest AudioKit

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DunneAudioKit",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "DunneAudioKit", targets: ["DunneAudioKit"])],
     dependencies: [
         .package(url: "https://github.com/AudioKit/KissFFT", from: "1.0.0"),
-        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.4.1"),
-        .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.4.0"),
+        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.5.0"),
+        .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.5.0"),
     ],
     targets: [
         .target(name: "DunneAudioKit", dependencies: ["AudioKit", "AudioKitEX", "CDunneAudioKit"]),
